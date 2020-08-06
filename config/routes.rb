1b43bda06d2a516917cc33ebc0d2b3977b70c3c8
yamachi0205 <yama.513421@gmail.com>
2020-08-06 18:20:11 +0900

    ログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   root "messages#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :delete]
 end
