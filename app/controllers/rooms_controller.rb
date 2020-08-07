a1c16b0f692f50172b9a8e571dbe13039ad2a7d5
yamachi0205 <yama.513421@gmail.com>
2020-08-07 15:30:26 +0900

    チャット画面の編集

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -16,7 +16,7 @@ class RoomsController < ApplicationController
   private
 
   def room_params
-    params.require(:room).permit(:name, user_id:[])
+    params.require(:room).permit(:name, user_ids:[])
   end
 
 end
